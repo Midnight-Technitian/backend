fd6ef286d6aaf9035b4305ea58ef5fcb00cf42b4
Glabay <Java_Guru@live.com>
2025-10-21 13:20:24 -0300

    Initial Commit

---
+++ services/src/main/java/dev/glabay/ServicesApplication.java
@@ -0,0 +1,13 @@
+package dev.glabay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ServicesApplication.class, args);
+    }
+
+}
