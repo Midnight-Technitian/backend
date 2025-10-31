18d1d9a6221c34f26078b4160c3c2499b50432e5
Glabay <Java_Guru@live.com>
2025-10-31 16:36:02 -0300

    removing trailing repos
    Signed-off-by: Glabay <Java_Guru@live.com>

--- services/src/main/java/dev/glabay/features/customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.glabay.features.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Optional<Customer> findByEmailIgnoreCase(String email);
-}
