7d9dd343ef167c8055ec3af96006a113c7f3bca2
Glabay <Java_Guru@live.com>
2025-11-01 22:10:05 -0300

    Intellij being weird about the Java 25 missing "public" in the PSVM method
    Signed-off-by: Glabay <Java_Guru@live.com>

--- services/src/main/java/dev/glabay/Services.java
+++ services/src/main/java/dev/glabay/Services.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Services {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SentryConfig.initialize();
         SpringApplication.run(Services.class, args);
     }
