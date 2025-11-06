83f163a56f1d9e9671430256c4069b177caacb90
Glabay <Java_Guru@live.com>
2025-11-05 23:36:47 -0400

    Moved service ticket requests to ticket controller
    Signed-off-by: Glabay <Java_Guru@live.com>

--- website/src/main/java/dev/glabay/controllers/TicketController.java
+++ website/src/main/java/dev/glabay/controllers/TicketController.java
@@ -1,15 +1,16 @@
 package dev.glabay.controllers;
 
-import dev.glabay.kafka.KafkaTopics;
-import dev.glabay.kafka.events.ServiceTicketCreationEvent;
+import dev.glabay.dtos.ServiceTicketDto;
+import dev.glabay.models.ServiceNote;
 import dev.glabay.models.request.ServiceRequest;
+import dev.glabay.services.KafkaEventService;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NullMarked;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 /**
  * @author Glabay | Glabay-Studios
@@ -20,14 +21,77 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @NullMarked
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/tickets")
+@RequestMapping("/api/service-ticket")
 public class TicketController {
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaEventService kafkaEventService;
+    private final RestClient restClient;
 
-    @PostMapping
+    @PostMapping("/tickets")
     private String postNewTicket(@RequestBody ServiceRequest body) {
-        var event = new ServiceTicketCreationEvent(body);
-        kafkaTemplate.send(KafkaTopics.SERVICE_TICKET_CREATION.getTopicName(), body.customerEmail(), event);
+        kafkaEventService.publishServiceTicketRequest(body);
         return "redirect:/dashboard";
+    }
+
+    @PostMapping()
+    private String updateServiceTicket(@RequestBody ServiceTicketDto body) {
+        var serviceNoteDto = restClient.put()
+            .uri("http://localhost:8081/api/v1/tickets")
+            .body(body)
+            .retrieve()
+            .toEntity(new ParameterizedTypeReference<ServiceTicketDto>() {})
+            .getBody();
+
+        if (serviceNoteDto == null)
+            return "redirect:/error";
+
+        return "redirect:/dashboard/ticket?id=".concat(serviceNoteDto.getTicketId());
+    }
+
+    @PostMapping("/notes")
+    private String postNewServiceTicketNotes(@RequestBody ServiceNote body) {
+        var serviceNoteDto = restClient.post()
+            .uri("http://localhost:8081/api/v1/tickets/note")
+            .body(body)
+            .retrieve()
+            .toEntity(new ParameterizedTypeReference<ServiceTicketDto>() {})
+            .getBody();
+
+        if (serviceNoteDto == null)
+            return "redirect:/error";
+
+        return "redirect:/dashboard/ticket?id=".concat(serviceNoteDto.getTicketId());
+    }
+
+    @PostMapping("/close")
+    private String closeServiceTicket(@RequestParam("ticketId") String ticketId) {
+        // TODO: Validate the provided String matches the expect pattern: 'TICKET-1'
+        var responseSpec = restClient.put()
+            .uri("http://localhost:8081/api/v1/tickets/close?ticketId={ticketId}", ticketId)
+            .retrieve();
+
+        if (responseSpec.toBodilessEntity().getStatusCode() != HttpStatus.OK)
+            return "redirect:/error";
+
+        return "redirect:/dashboard/ticketing";
+    }
+
+    @PostMapping("/assign")
+    private String claimServiceTicket(
+        @RequestParam("employeeId") String employeeId,
+        @RequestParam("ticketId") String ticketId
+    ) {
+        // TODO: Validate the provided String matches the expect pattern: 'TICKET-1', 'EMPLOYEE-1'
+        var responseSpec = restClient.put()
+            .uri("http://localhost:8081/api/v1/tickets/claim?" +
+                    "ticketId={ticketId}&" +
+                    "employeeId={employeeId}",
+                ticketId,
+                employeeId
+            ).retrieve();
+
+        if (responseSpec.toBodilessEntity().getStatusCode() != HttpStatus.OK)
+            return "redirect:/error";
+
+        return "redirect:/dashboard/ticket?id=".concat(ticketId);
     }
 }
--- website/src/main/resources/static/js/ServiceTicket.js
+++ website/src/main/resources/static/js/ServiceTicket.js
@@ -18,7 +18,7 @@ function submitNewTicket(event) {
         serviceDescription:form.description.value
     };
 
-    fetch("/api/tickets", {
+    fetch("/api/service-ticket/tickets", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(data)
