c2bce12bcf59e09b2534f83a9efc24153fed719f
Glabay <Java_Guru@live.com>
2025-11-05 23:37:33 -0400

    Added Kafka publishing event
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ website/src/main/java/dev/glabay/services/KafkaEventService.java
@@ -0,0 +1,46 @@
+package dev.glabay.services;
+
+import dev.glabay.dtos.UserProfileDto;
+import dev.glabay.kafka.KafkaTopics;
+import dev.glabay.kafka.events.device.CustomerDeviceRegistrationEvent;
+import dev.glabay.kafka.events.ticket.ServiceTicketCreationEvent;
+import dev.glabay.kafka.events.user.UserRegisteredEvent;
+import dev.glabay.logging.MidnightLogger;
+import dev.glabay.models.device.RegisteringDevice;
+import dev.glabay.models.request.ServiceRequest;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project midnight-technician
+ * @social Discord: Glabay
+ * @since 2025-11-05
+ */
+@Service
+@RequiredArgsConstructor
+public class KafkaEventService {
+    private final Logger logger = MidnightLogger.getLogger(KafkaEventService.class);
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishUserRegistration(UserProfileDto dto, String ipAddress) {
+        var event = new UserRegisteredEvent(dto, ipAddress);
+        kafkaTemplate.send(KafkaTopics.USER_REGISTRATION.getTopicName(), dto.email(), event);
+        logger.info("User Registered Event sent to Kafka {}", event);
+    }
+
+    public void publishDeviceRegistration(RegisteringDevice body) {
+        var event = new CustomerDeviceRegistrationEvent(body);
+        kafkaTemplate.send(KafkaTopics.CUSTOMER_DEVICE_REGISTRATION.getTopicName(), body.getCustomerEmail(), event);
+        logger.info("New Customer Device Registration Event sent to Kafka {}", event);
+    }
+
+    public void publishServiceTicketRequest(ServiceRequest body) {
+        var event = new ServiceTicketCreationEvent(body);
+        kafkaTemplate.send(KafkaTopics.SERVICE_TICKET_CREATION.getTopicName(), body.customerEmail(), event);
+        logger.info("New Service Ticket Creation Event sent to Kafka {}", event);
+    }
+}
